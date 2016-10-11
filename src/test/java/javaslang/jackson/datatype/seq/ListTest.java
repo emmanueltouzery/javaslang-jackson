6fbad5f8a0bbeb18134b0fe5ce30b81d8ec860cc
Emmanuel Touzery <etouzery@gmail.com>
2016-10-11 15:36:33 +0200

    add failing tests related to XML-specific jackson annotations

--- src/test/java/javaslang/jackson/datatype/seq/ListTest.java
+++ src/test/java/javaslang/jackson/datatype/seq/ListTest.java
@@ -1,6 +1,15 @@
 package javaslang.jackson.datatype.seq;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -37,6 +46,56 @@ public class ListTest extends SeqTest {
     @Test
     public void testDefaultDeserialization() throws IOException {
         Assert.assertEquals(mapper().readValue("[1]", Seq.class), List.of(1));
+    }
+
+    @XmlRootElement(name = "xmlSerialize")
+    private static class JaxbXmlSerializeJavaslang {
+        @XmlElementWrapper(name = "transitTypes")
+        @XmlElement(name = "transitType")
+        public List<Integer> transitTypes = List.of(1, 2, 3);
+    }
+
+    @XmlRootElement(name = "xmlSerialize")
+    private static class JaxbXmlSerializeJavaUtil {
+        @XmlElementWrapper(name = "transitTypes")
+        @XmlElement(name = "transitType")
+        public java.util.List<Integer> transitTypes = new java.util.ArrayList<>();
+
+        public JaxbXmlSerializeJavaUtil() {
+            transitTypes.add(1);
+            transitTypes.add(2);
+            transitTypes.add(3);
+        }
+    }
+
+    @Test
+    public void testJaxbXmlSerialization() throws IOException {
+        String javaUtilValue = xmlMapperJaxb().writeValueAsString(new JaxbXmlSerializeJavaslang());
+        Assert.assertEquals(xmlMapperJaxb().writeValueAsString(new JaxbXmlSerializeJavaUtil()), javaUtilValue);
+    }
+
+    private static class XmlSerializeJavaslang {
+        @JacksonXmlElementWrapper(localName = "transitTypes")
+        @JsonProperty("transitType")
+        public List<Integer> transitTypes = List.of(1, 2, 3);
+    }
+
+    private static class XmlSerializeJavaUtil {
+        @JacksonXmlElementWrapper(localName = "transitTypes")
+        @JsonProperty("transitType")
+        public java.util.List<Integer> transitTypes = new java.util.ArrayList<>();
+
+        public XmlSerializeJavaUtil() {
+            transitTypes.add(1);
+            transitTypes.add(2);
+            transitTypes.add(3);
+        }
+    }
+
+    @Test
+    public void testXmlSerialization() throws IOException {
+        String javaUtilValue = xmlMapper().writeValueAsString(new XmlSerializeJavaslang());
+        Assert.assertEquals(xmlMapper().writeValueAsString(new XmlSerializeJavaUtil()), javaUtilValue);
     }
 
 }
